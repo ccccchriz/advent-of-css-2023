f345a27f8d625de7ba56dc7454262bc25c59251a
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-18 16:38:53 +0100

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Authentication from "./pages/authentication/Authentication.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Authentication type="login" />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
