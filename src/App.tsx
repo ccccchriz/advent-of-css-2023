c03132330aa04f9e4c077d524c7f8f69dc6b6a90
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-19 16:51:14 +0100

    added invite group form, day 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,10 @@
-import InviteCard from "./components/User/InviteCard";
+import InviteGroupForm from "./pages/main/InviteGroupForm";
 
 function App() {
   return (
     <>
-      <div className="grid grid-cols-2 bg-red-500 gap-12 py-12 px-4">
-        <InviteCard
-          letter="S"
-          status="online"
-          name="Stever Rogers"
-          mail="captainamerica24@avengers.com"
-        />
-
-        <InviteCard
-          letter="T"
-          status="do-not-disturb"
-          name="Tony Stark"
-          mail="ironmanthefirst@avengers.com"
-        />
-
-        <InviteCard
-          letter="T"
-          status="idle"
-          name="Miles Morales"
-          mail="spidey@avengers.com"
-        />
-        <InviteCard
-          letter="S"
-          status="online"
-          name="Stever Rogers"
-          mail="captainamerica24@avengers.com"
-        />
-
-        <InviteCard
-          letter="T"
-          status="do-not-disturb"
-          name="Tony Stark"
-          mail="ironmanthefirst@avengers.com"
-        />
-
-        <InviteCard
-          letter="T"
-          status="idle"
-          name="Miles Morales"
-          mail="spidey@avengers.com"
-        />
+      <div className="bg-silverTree p-16">
+        <InviteGroupForm />
       </div>
     </>
   );
---
+++ src/pages/main/InviteGroupForm.tsx
@@ -0,0 +1,81 @@
+import Input from "../../components/Input/Input";
+import InviteCard from "../../components/User/InviteCard";
+
+export default function InviteGroupForm() {
+  return (
+    <>
+      <div className="">
+        <h2 className="text-white font-handwriting uppercase mb-2 text-lg">
+          invite a friend or family member
+        </h2>
+        <form className="flex flex-col lg:flex-row gap-4 w-full bg-spanishGreen p-4">
+          <div className="grow">
+            <Input type="text" />
+          </div>
+          <div className="grow">
+            <Input type="email" />
+          </div>
+          <button
+            type="submit"
+            className="w-16 p-4 h-min rounded-full aspect-square bg-supernova self-center border-2 border-white"
+          >
+            <span className="sr-only"></span>
+            <span aria-hidden="true" className="">
+              +
+            </span>
+          </button>
+        </form>
+        <div className="grid lg:grid-cols-2 gap-12 py-12 px-4">
+          <InviteCard
+            letter="S"
+            status="online"
+            name="Stever Rogers"
+            mail="captainamerica24@avengers.com"
+          />
+          <InviteCard
+            letter="T"
+            status="do-not-disturb"
+            name="Tony Stark"
+            mail="ironmanthefirst@avengers.com"
+          />
+          <InviteCard
+            letter="T"
+            status="idle"
+            name="Miles Morales"
+            mail="spidey@avengers.com"
+          />
+          <InviteCard
+            letter="S"
+            status="online"
+            name="Stever Rogers"
+            mail="captainamerica24@avengers.com"
+          />
+          <InviteCard
+            letter="T"
+            status="do-not-disturb"
+            name="Tony Stark"
+            mail="ironmanthefirst@avengers.com"
+          />
+          <InviteCard
+            letter="T"
+            status="idle"
+            name="Miles Morales"
+            mail="spidey@avengers.com"
+          />
+          <InviteCard
+            letter="T"
+            status="do-not-disturb"
+            name="Tony Stark"
+            mail="ironmanthefirst@avengers.com"
+          />
+          <InviteCard
+            letter="T"
+            status="idle"
+            name="Miles Morales"
+            mail="spidey@avengers.com"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
