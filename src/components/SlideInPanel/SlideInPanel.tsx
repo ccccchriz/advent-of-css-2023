4284bb8b167a9fbfe3f180508cc6535518485117
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-21 19:15:32 +0100

    slidein panel closes when click outside of it

--- src/components/SlideInPanel/SlideInPanel.tsx
+++ src/components/SlideInPanel/SlideInPanel.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useRef, ReactNode } from "react";
-import MenuButton from "../Menu/MenuButton";
 import { Icon } from "../Icon/Icon";
 
 type SlideInPanelProps = {
@@ -9,31 +8,32 @@ type SlideInPanelProps = {
 function SlideInPanel({ children }: SlideInPanelProps) {
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const button = useRef<HTMLButtonElement>(null);
+  const panel = useRef<HTMLDivElement>(null);
+
+  const handleKeyboardDisableMenu = (e: KeyboardEvent) => {
+    if (isExpanded == true && e.key == "Escape") {
+      setIsExpanded(false);
+    }
+  };
+
+  const handleMouseDisableMenu = (e: MouseEvent) => {
+    if (panel.current && !panel.current.contains(e.target as Node))
+      setIsExpanded(false);
+  };
 
   useEffect(() => {
-    const handleDisableMenu = (e: KeyboardEvent) => {
-      if (isExpanded == true && e.key == "Escape") {
-        if (button.current != null) button.current.focus();
-        setIsExpanded(false);
-      }
+    document.addEventListener("keydown", handleKeyboardDisableMenu);
+    document.addEventListener("click", handleMouseDisableMenu);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardDisableMenu);
+      document.removeEventListener("click", handleMouseDisableMenu);
     };
-
-    document.addEventListener("keydown", handleDisableMenu);
-
-    return () => document.removeEventListener("keydown", handleDisableMenu);
   }, [isExpanded]);
 
   return (
     <>
-      <div className="lg:max-w-md grid absolute">
-        <div className={`p-4 z-50 left-0 w-min`}>
-          <MenuButton
-            isExpanded={isExpanded}
-            setIsExpanded={setIsExpanded}
-            ref={button}
-          />
-        </div>
+      <div className="lg:max-w-md grid absolute" ref={panel}>
         <div
           className={`right-0 w-full md:max-w-[45rem] min-h-[100dvh] bg-spanishGreen z-40 fixed top-0 flex flex-col transition-all duration-300 ${
             isExpanded ? "panelSlideIn" : "panelSlideIn--reverse"
