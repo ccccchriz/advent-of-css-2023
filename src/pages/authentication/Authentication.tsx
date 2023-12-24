d49180921f47c9ffc442a33bbc26af919e4cc707
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-24 09:11:19 +0100

    updated auth component

--- src/pages/authentication/Authentication.tsx
+++ src/pages/authentication/Authentication.tsx
@@ -1,23 +1,24 @@
+import { ScrollRestoration } from "react-router-dom";
 import Footer from "../../components/Footer/Footer.tsx";
-import { authComponents } from "./authComponents.tsx";
-import ScrollToTop from "../../utilities/ScrollToTop.tsx";
+import { ReactNode } from "react";
 
 type authProps = {
-  type: keyof typeof authComponents;
+  children: ReactNode;
+  title?: string;
 };
 
-export default function Authentication({ type }: authProps) {
+export default function Authentication({ title, children }: authProps) {
   return (
     <>
-      <ScrollToTop />
+      <ScrollRestoration />
       <main className="w-full min-h-screen lg:bg-[url('/website/bg__auth.svg')] bg-cover bg-no-repeat grid place-items-center bg-silverTree dark:bg-nileBlue pt-4">
         <section className="grid place-items-center gap-4 w-full max-w-3xl px-4">
           <h1 className="sr-only">Secret Santa</h1>
           <img src="/website/logo__secret-santa.svg" alt="" />
           <h2 className="flex items-center gap-3 text-white text-6xl font-condensed uppercase before:block before:h-1 before:w-12 before:bg-white after:block after:h-1 after:w-12 after:bg-white text-center">
-            {authComponents[type].title}
+            {title}
           </h2>
-          {authComponents[type].node}
+          {children}
         </section>
       </main>
       <Footer />
